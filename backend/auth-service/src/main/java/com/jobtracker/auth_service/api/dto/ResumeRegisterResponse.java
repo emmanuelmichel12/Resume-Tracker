ef6afa6633501c5d55f4ee47be098f066dd2ed8c
Emmanuel Michel <emmanuel.michel12@gmail.com>
2026-06-04 14:59:18 -0400

    Initial commit

---
+++ backend/auth-service/src/main/java/com/jobtracker/auth_service/api/dto/ResumeRegisterResponse.java
@@ -0,0 +1,64 @@
+package com.jobtracker.auth_service.api.dto;
+
+import java.time.LocalDateTime;
+
+public class ResumeRegisterResponse {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private LocalDateTime createdAt;
+
+    public ResumeRegisterResponse() {
+    }
+
+    public ResumeRegisterResponse(long id, String firstName, String lastName, String username,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ backend/auth-service/src/main/java/com/jobtracker/auth_service/model/Users.java
@@ -0,0 +1,105 @@
+package com.jobtracker.auth_service.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "authService")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstname", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "lastname", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 300, unique = true)
+    private String email;
+
+    @Column(name = "username", nullable = false, length = 100, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 250)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Users() {
+
+    }
+
+    public Users(long id, String firstName, String lastName, String email, String username, String password,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
