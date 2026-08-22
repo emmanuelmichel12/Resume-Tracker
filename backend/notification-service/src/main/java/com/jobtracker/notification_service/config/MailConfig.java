5df3ae430ff8ea5f498685a701b98104a77b5a28
Emmanuel Michel <emmanuel.michel12@gmail.com>
2026-08-21 22:04:39 -0400

    Modified MailConfig

--- backend/notification-service/src/main/java/com/jobtracker/notification_service/config/MailConfig.java
+++ backend/notification-service/src/main/java/com/jobtracker/notification_service/config/MailConfig.java
@@ -11,25 +11,16 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
+    @Value("${RESEND_API_KEY}")
+    private String apiKey;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost("smtp.resend.com");
+        mailSender.setPort(465);
+        mailSender.setUsername("resend");
+        mailSender.setPassword(apiKey);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
